3de77b134c98c9a22f24970bca99502349919c62
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2024-11-26 13:46:18 -0800

    Time: 145 ms (12.38%), Space: 26.5 MB (37.6%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,29 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        minValue = float('inf')  # Start with a very large value
-        maxProfit = 0  # To store the maximum profit
+        
 
-        array = []
-        minV = 0
+        minValue = prices[0]
+        profit = 0
 
-        # Find the minimum value and its index
         for i in range(len(prices)):
-            if prices[i] < minValue:
-                minValue = prices[i]
-                minV = i  # Update the index of the minimum value
+            profit = max(profit, prices[i] - minValue)
+            minValue = min(minValue, prices[i])
 
-        # Collect all prices after the minimum value
-        for i in range(minV, len(prices)):
-            if prices[i] > minValue:
-                array.append(prices[i])
-
-        # Calculate the maximum profit
-        if array:
-            maxValue = max(array)
-            maxProfit = maxValue - minValue
-
-        return maxProfit
-
+        return profit
                 
 
 
