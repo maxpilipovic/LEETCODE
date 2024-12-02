57f140d25cf7e1c1f0376912c2e193af34956e92
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2024-12-02 15:50:25 -0800

    Time: 56 ms (97.72%), Space: 91 MB (77.42%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -1,35 +1,38 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def minDepth(self, root: Optional[TreeNode]) -> int:
-
+from collections import deque
+class Solution(object):
+    def minDepth(self, root):
+        
         if not root:
             return 0
 
-        #dfs
-        stack = [(root, 1)]
-        min_level = float('inf')
+        q = deque()
+        q.append(root)
 
-        while stack:
-            n = len(stack)
-            node, level = stack.pop()
+        level = 1
+        while q:
+            n = len(q)
 
-            if not node.left and not node.right:
-                min_level = min(min_level, level)
-                 
-            if node.left:
-                stack.append((node.left, level + 1))
-            if node.right:
-                stack.append((node.right, level + 1))
+            for _ in range(n):
+                node = q.popleft()
+
+                if not node.left and not node.right:
+                    return level
+
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            level += 1
         
-        return min_level
-                    
+        return level
 
-
-
-
+        
+        
         
