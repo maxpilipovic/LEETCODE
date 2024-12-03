1bc020650a756ee083d40239e5f4c5d10c28b309
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2024-12-03 07:44:44 -0800

    Time: 11 ms (80.09%), Space: 20 MB (55.4%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,23 +1,18 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        #create dict
         my_dict = dict()
 
-        for i in strs:
-            key = ''.join(sorted(i))
 
-            if key in my_dict:
-                my_dict[key].append(i)
+        for i in strs:
+            sorted_word = ''.join(sorted(i))
+            if sorted_word in my_dict:
+                #add to list
+                my_dict[sorted_word].append(i)
             else:
-                my_dict[key] = [i]
+                my_dict[sorted_word] = [i]
         
         return list(my_dict.values())
 
-
-            
-
-    
-
-        
-
-        
         
