49b93e212c1727fcc9800155737a5c35056a6745
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2024-12-18 17:30:53 +1100

    Time: 3 ms (55.13%), Space: 17.8 MB (10.22%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -1,12 +1,15 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
         
-        array = prices.copy()
-        
+        array = []
+
         for i in range(len(prices)):
+            array.append(prices[i])
+
+            #look for price
             for j in range(i + 1, len(prices)):
                 if prices[j] <= prices[i]:
-                    array[i] = prices[i] - prices[j]
+                    array[i] = (prices[i] - prices[j])
                     break
         
         return array
