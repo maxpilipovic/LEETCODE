4e350e8516690fe0841cbadb3f3ce47d1e237bd5
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-01-15 05:47:34 +1100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,38 +1,37 @@
-class Solution(object):
-    def checkInclusion(self, s1, s2):
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
 
-        my_dict = dict()
-        window_dict = dict()
+        if len(s1) > len(s2):
+            return False
 
-        window_length = len(s1)
-        string1 = sorted(s1)
-        string2 = sorted(s2)
+        s1_dict = dict()
+        s2_dict = dict()
 
         for char in range(len(s1)):
-            if s1[char] in my_dict:
-                my_dict[s1[char]] += 1
-            else:
-                my_dict[s1[char]] = 1
+            s1_dict[s1[char]] = 1 + s1_dict.get(s1[char], 0)
+            s2_dict[s2[char]] = 1 + s2_dict.get(s2[char], 0)
+        
+        #check if they are the same, after first two elements
+        if s1_dict == s2_dict:
+            return True
 
+        #sliding window part
         left = 0
 
-        for right in range(len(s2)):
+        for right in range(len(s1), len(s2)):
+            #adds 1 to the value of s2[right], default value 0 if not present
+            s2_dict[s2[right]] = 1 + s2_dict.get(s2[right], 0)
+            #remove left
+            del s2_dict[s2[left]]
 
-            current_char = s2[right]
-            window_dict[current_char] = window_dict.get(current_char, 0) + 1
+            #increment left
+            left += 1
 
-            if right >= window_length:
-                left_char = s2[right - window_length]
-                if window_dict[left_char] == 1:
-                    del window_dict[left_char]
-                else:
-                    window_dict[left_char] -= 1
-            
-            if my_dict == window_dict:
+            #check if they are the same
+            if s1_dict == s2_dict:
                 return True
-    
+        
         return False
-
 
 
 
