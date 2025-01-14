225e8ba7fdf4156ecce345937a24f88e52d67db2
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-01-14 14:05:17 +1100

    Time: 0 ms (100%), Space: 16.7 MB (87.16%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,23 +1,25 @@
-class Solution(object):
-    def climbStairs(self, n):
+class Solution:
+    def climbStairs(self, n: int) -> int:   
 
-        my_dict = dict()
+        my_dict = {}
 
         def recursion(n):
             if n == 0:
                 return 0
-
             if n == 1:
                 return 1
-        
+
             if n == 2:
                 return 2
 
+            #if its computed
             if n in my_dict:
                 return my_dict[n]
-                
-            my_dict[n] = recursion(n-1) + recursion(n-2)
-            
+
+            #if its not computed
+            my_dict[n] = recursion(n - 1) + recursion(n - 2)
+
+            #return
             return my_dict[n]
 
         return recursion(n)
