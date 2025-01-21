ed58cc7f3e35b578c4c64af11eafc500bfe17286
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-01-22 10:54:58 +1100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,22 +1,22 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        if len(s) != len(t):
+        if len(s) > len(t):
             return False
-
         my_dict = dict()
 
         for i in range(len(s)):
-            if s[i] not in my_dict:
-                my_dict[s[i]] = 1
-            else:
+            if s[i] in my_dict:
                 my_dict[s[i]] += 1
+            else:
+                my_dict[s[i]] = 1
 
         for i in range(len(t)):
-            if t[i] in my_dict and my_dict[t[i]] > 0:
-                my_dict[t[i]] -= 1
+            if t[i] in my_dict:
+                my_dict[s[i]] -= 1
             else:
                 return False
+        
         return True
 
         
