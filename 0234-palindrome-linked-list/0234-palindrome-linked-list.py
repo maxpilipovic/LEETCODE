3ed01850a80e09f4920f22cb625192fe574ec115
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-01-23 11:21:36 -0800

    Time: 47 ms (23.77%), Space: 30.2 MB (97.2%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,19 +5,15 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        
-        #empty
-        if not head or not head.next:
-            return True
-        
-        #find mid point
-        fast = head
+
         slow = head
+        fast = head
+        #Find the middle point
         while fast != None and fast.next != None:
             slow = slow.next
             fast = fast.next.next
 
-        #reverse first half
+        #Reverse list
         prev = None
         curr = head
         while curr != slow:
@@ -25,16 +21,16 @@ class Solution:
             curr.next = prev
             prev = curr
             curr = nxt
-
-
-        #handles odd-length (skip middle)
-        #if Fast is None, odd list.
+        
+        #Odd length
+        #If fast is None, then odd.
         if fast:
             slow = slow.next
-
+        
         #compare
         left = prev
-        right = slow 
+        right = slow
+
         while left and right != None:
             if left.val != right.val:
                 return False
@@ -42,14 +38,5 @@ class Solution:
             right = right.next
         return True
 
-
-
-
-
-        
-
-        
-        
-        
 
         
