b5c738af8968077f42c7f02197095bc66b4b091a
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-01-30 16:39:33 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -5,43 +5,32 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        
 
         #If empty
         if head is None and left == 1 and right == 1:
             return head
 
-        array = []
-        curr = head
-        
-        #Loops and adds values to res array
-        while curr != None:
-            array.append(curr.val)
-            curr = curr.next 
-        
-        res = []
-        #Do the left part
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+
         for i in range(0, left - 1):
-            res.append(array[i])
+            prev = prev.next
         
-        middle = []
-        #Do the middle part
-        middle = array[left - 1: right]
-        middle.reverse()
-        res.extend(middle)
 
-        #Do the right part
-        for i in range(right, len(array), 1):
-            res.append(array[i])
+        #Start reversing from prev
+        curr = prev.next
 
-        curr2 = head
-        counter = 0
-        while curr2 != None:
-            curr2.val = res[counter]
-            counter += 1
-            curr2 = curr2.next
+        #Loops for (right - left) times
+        for _ in range(right - left):
+            temp = curr.next
+            curr.next = temp.next
+            temp.next = prev.next
+            prev.next = temp
 
-        return head
+
+
+
 
         
 
