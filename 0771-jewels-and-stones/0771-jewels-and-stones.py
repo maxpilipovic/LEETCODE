5f73fcec526d562c00baeb88cd1447cd820bd3c6
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-02-07 15:08:21 -0600

    Time: 0 ms (100%), Space: 17.8 MB (47.12%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+
+        my_dict = dict()
+
+        for i in range(len(jewels)):
+            if jewels[i] in my_dict:
+                my_dict[jewels[i]] += 1
+            else:
+                my_dict[jewels[i]] = 1
+
+        count = 0
+        for i in range(len(stones)):
+            if stones[i] in my_dict:
+                count += 1
+
+        return count
+        
