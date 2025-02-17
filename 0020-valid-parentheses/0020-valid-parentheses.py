4521b2ed0d0f4f3b405c8a07c9e761aaea7aaa3c
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-02-17 14:45:37 -0600

    Time: 3 ms (21.72%), Space: 17.8 MB (80.69%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,29 +1,44 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-
         stack = []
-        mapping = {
-            ')': '(',
-            '}': '{',
-            ']': '['
-        }
+        openCount = 0
+        openCount2 = 0
+        openCount3 = 0
 
-
-        for char in s:
-
-            if char == '(' or char == '{' or char == '[':
-                stack.append(char)
-
-            elif char == ')' or char == '}' or char == ']':
-
-                if not stack:
-                    return False
+        for i in s:
+            if i == "(":
+                stack.append(i)
+                openCount += 1
+            elif i == "{":
+                stack.append(i)
+                openCount2 += 1
+            elif i == "[":
+                stack.append(i)
+                openCount3 += 1
+            elif i == ")":
+                if openCount > 0 and stack and stack[-1] == "(":
+                    stack.pop()
+                    openCount -= 1
                 else:
-                    top = stack.pop()
-                    if (mapping[char] != top):
-                        return False
+                    return False
+            elif i == "}":
+                if openCount2 > 0 and stack and stack[-1] == "{":
+                    stack.pop()
+                    openCount2 -= 1
+                else:
+                    return False
+            elif i == "]":
+                if openCount3 > 0 and stack and stack[-1] == "[":
+                    stack.pop()
+                    openCount3 -= 1
+                else:
+                    return False
+        
+        return openCount == 0 and openCount2 == 0 and openCount3 == 0
 
-        return not stack
+
+        
+
 
 
         
