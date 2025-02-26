5b1cfc6d0ba6301ebc5f49c7715eec6411959e0e
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-02-26 11:18:56 -0600

    Time: 147 ms (45.85%), Space: 31.8 MB (95.02%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -8,23 +8,22 @@ class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         
         count = 0
-        
-        def dfs(node, maxVal, count):
-            #Base
-            if not node:
-                return count
-            
-            #Check
+        maxVal = root.val
+        stack = [(root, maxVal)]
+
+        while stack:
+
+            node, maxVal = stack.pop()
+
             if node.val >= maxVal:
                 count += 1
 
             #Update our node.val to compare
             maxVal = max(maxVal, node.val)
             
-            #Recursive
-            count = dfs(node.left, maxVal, count)
-            count = dfs(node.right, maxVal, count)
+            if node.left:
+                stack.append((node.left, maxVal))
+            if node.right:
+                stack.append((node.right, maxVal))
 
-            return count
-        
-        return dfs(root, root.val, count)
+        return count
