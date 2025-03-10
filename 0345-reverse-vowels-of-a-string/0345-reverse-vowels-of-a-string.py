5496eea869ae17d15da3a0719e5fd0de983b156a
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-03-10 18:25:20 -0500

    Time: 7 ms (91.5%), Space: 18.6 MB (70.03%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -1,25 +1,29 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-
-        hashy = set("aeiouAEIOU")
-        s = list(s)
+    
+        my_dict = {'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1}
+        
+        res = list(s)
         left = 0
-        right = len(s) - 1
+        right = len(res) - 1
 
         while left < right:
-            if s[left] not in hashy:
-                left += 1
-            if s[right] not in hashy:
-                right -= 1
 
-            if s[right] in hashy and s[left] in hashy:
-                s[left], s[right] = s[right], s[left]
+            if res[left] in my_dict and res[right] in my_dict:
+                #Swap
+                res[left], res[right] = res[right], res[left]
+
+                #Increment both
                 left += 1
                 right -= 1
         
-        return "".join(s)
+            if res[left] not in my_dict:
+                left += 1
+        
+            if res[right] not in my_dict:
+                right -= 1
 
-
+        return ''.join(res)
 
 
 
