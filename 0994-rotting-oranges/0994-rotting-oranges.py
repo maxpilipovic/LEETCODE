b6e5fe09bd7435d8e01bc87e3a3f32bbac2b8450
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-03-31 18:57:37 -0500

    Time: 3 ms (82.11%), Space: 18 MB (27.21%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,46 +1,47 @@
 from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        print(len(grid))
-        print(len(grid[0]))
+
         rows = len(grid)
         cols = len(grid[0])
 
-        queue = deque()
-        freshOranges = 0
+        queue = deque() #Keep track of our rotten oranges (r, c, 0)
+        freshOranges = 0 #Keep track fresh oranges
 
-        #1. Put all rotten oranges in queue
-
+        #1 Initialized our queue and kept track of fresh oranges 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 2:
-                    print(r, c)
-                    queue.append((r, c, 0)) #row, col, min
+                    queue.append((r, c, 0))
                 elif grid[r][c] == 1:
                     freshOranges += 1
         
-        #2: Loop through using bfs and make all oranges rotten
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)] #Right, Left, Down Up
+        #2 Loop through using bfs and make all oranges rotten
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         time = 0
 
         while queue:
 
-            row, col, minute = queue.popleft()
-            
+            row, col, minutes = queue.popleft()
+
             #Update our time
-            time = max(time, minute)
+            time = max(time, minutes)
 
             for dr, dc in directions:
                 nr = dr + row
                 nc = dc + col
 
-                if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1: #Fresh orange
-
-                    grid[nr][nc] = 2 #Rotten Orange
+                if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
+                    grid[nr][nc] = 2
                     freshOranges -= 1
-                    queue.append((nr, nc, minute + 1))
-                
-            
+                    queue.append((nr, nc, minutes + 1))
+
+        
         return time if freshOranges == 0 else -1
+        
+
+                
 
 
+
+        
