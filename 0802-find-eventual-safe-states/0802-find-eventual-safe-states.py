d8e8ba37ad8d78eccdae022b6f4f87f625f79507
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-04-05 14:05:51 -0500

    Time: 354 ms (5.02%), Space: 24 MB (24.54%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -0,0 +1,56 @@
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+
+        def adjList(graph):
+
+            adjList = {}
+            
+            for node in range(len(graph)):
+                if node not in adjList:
+                    adjList[node] = []
+                for dest in graph[node]:
+                    adjList[node].append(dest)
+
+            return adjList
+
+        def dfs(node, adjList, visited, res):
+
+            #Base Case
+            if node in visited: #Found a cycle
+                return False
+            
+            if node in res:
+                return True
+
+            visited.add(node)
+
+            #Recursive
+            for neighbor in adjList[node]:
+                if not dfs(neighbor, adjList, visited, res):
+                    return False
+            
+            visited.remove(node) #Node is processed
+            res.add(node)
+            return True
+
+
+        adjList = adjList(graph)
+        res = set()
+
+        #Adds initial terminal nodes
+        for i in adjList:
+            if adjList[i] == []:
+                res.add(i)
+    
+        for node in range(len(graph)):
+            visited = set()
+            dfs(node, adjList, visited, res)
+        
+        return sorted(res)
+
+        
+
+    
+
+
+        
