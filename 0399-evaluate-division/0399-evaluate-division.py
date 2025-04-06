a0f083e4229108a0687749b6e343c47150778f7e
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-04-06 14:32:07 -0500

    Time: 0 ms (100%), Space: 17.8 MB (94.28%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,22 +1,25 @@
-from collections import deque
+from collections import deque, defaultdict
+
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         
-        adj = collections.defaultdict(list)
+        adj = defaultdict(list)
 
         for i, eq in enumerate(equations):
             a, b = eq
-            adj[a].append([b, values[i]])
-            adj[b].append([a, 1 / values[i]])
+            adj[a].append((b, values[i]))
+            adj[b].append((a, 1 / values[i]))
         
         def bfs(src, target):
-            if src not in adj and target not in adj:
-                return -1
+            if src not in adj or target not in adj:
+                return -1.0
+            if src == target:
+                return 1.0
             
             queue = deque()
             visited = set()
 
-            queue.append([src, 1])
+            queue.append((src, 1.0))
             visited.add(src)
 
             while queue:
@@ -25,10 +28,10 @@ class Solution:
                 if node == target:
                     return weight
                 
-                for neighbor, weight2 in adj[node]:
+                for neighbor, edge_weight in adj[node]:
                     if neighbor not in visited:
-                        queue.append((neighbor, weight * weight2))
+                        queue.append((neighbor, weight * edge_weight))
                         visited.add(neighbor)
-            return -1
+            return -1.0
 
         return [bfs(q[0], q[1]) for q in queries]
