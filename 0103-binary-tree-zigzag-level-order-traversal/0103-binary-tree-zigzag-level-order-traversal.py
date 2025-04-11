70f76a3ede558b872c8fd52d69da779935019c9f
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-04-11 16:56:14 -0500

    Time: 0 ms (100%), Space: 18.2 MB (8.82%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -4,47 +4,39 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 from collections import deque
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         
         if not root:
             return []
-
-        queue = deque([root])
+            
         res = []
-        count = 0
+        queue = deque([(root, 0)])
 
         while queue:
-            
-            curr = []
+
             length = len(queue)
+            curr = []
 
             for i in range(length):
 
-                node = queue.popleft()
-
-                #Add to curr
+                node, level = queue.popleft()
                 curr.append(node.val)
 
-                if i == length - 1:
-
-                    #If our level is odd
-                    if count % 2 != 0:
+                #We know this is the last value
+                if length - 1 == i:
+                    if level % 2 == 0:
+                        #Even
+                        res.append(curr.copy())
+                    else:
+                        #Odd
                         curr.reverse()
-                    
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
+                        res.append(curr.copy())
                 
-            res.append(curr)
-            count += 1
+                if node.left:
+                    queue.append((node.left, level + 1))
+                if node.right:
+                    queue.append((node.right, level + 1))
         
         return res
-            
-
-
-
-
