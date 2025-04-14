da53ad8954be5550684d244081fed3de331b8298
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-04-14 18:34:52 -0500

    Time: 11 ms (72.53%), Space: 17.9 MB (64.33%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,22 +1,19 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+
+        my_dict = {}
+
+        for i in s:
+            if i not in my_dict:
+                my_dict[i] = 1
+            else:
+                my_dict[i] += 1
         
-        if len(s) > len(t):
-            return False
-        my_dict = dict()
-
-        for i in range(len(s)):
-            if s[i] in my_dict:
-                my_dict[s[i]] += 1
-            else:
-                my_dict[s[i]] = 1
-
-        for i in range(len(t)):
-            if t[i] in my_dict:
-                my_dict[s[i]] -= 1
-            else:
+        for i in t:
+            if i not in my_dict:
                 return False
+            else:
+                my_dict[i] -= 1
         
-        return True
 
-        
+        return all(count == 0 for count in my_dict.values()) #Return true if the statement is true, else false... ALWAYS TRUE
