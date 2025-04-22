f13e4200a81d2d82afcb65ff3034ca8230645fc3
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-04-22 17:05:48 -0500

    Time: 15 ms (90.1%), Space: 18.2 MB (68.03%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -8,12 +8,13 @@ class Solution:
         directions = [(0, -1), (0, 1), (-1, 0), (1, 0)] #Up, Down, Right, Left
         print(rows)
         print(cols)
-        res = 0
+        maxCount = 0
 
-        def bfs(row, col, visited):
+        def bfs(row, col, visited, maxCount):
             queue = deque()
             queue.append((row, col, visited))
             visited.add((row, col))
+            count = 1
 
             while queue:
                 
@@ -27,14 +28,17 @@ class Solution:
                     if 0 <= newRow < rows and 0 <= newCol < cols and grid[newRow][newCol] == 1 and (newRow, newCol) not in visited:
                         visited.add((newRow, newCol))
                         queue.append((newRow, newCol, visited))
+                        count += 1
+            
+            return count
 
         for row in range(rows):
             for col in range(cols):
                 if grid[row][col] == 1 and (row, col) not in visited:
-                    res += 1
-                    bfs(row, col, visited)
+                    count = bfs(row, col, visited, maxCount)
+                    maxCount = max(maxCount, count)
         
-        return res
+        return maxCount
 
 
         
