7df713db3949742e6ddbfffb0b6f4a8559426c1f
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-04-22 17:20:40 -0500

    Time: 23 ms (59.51%), Space: 18.6 MB (33.02%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -10,32 +10,31 @@ class Solution:
         print(cols)
         maxCount = 0
 
-        def bfs(row, col, visited, maxCount):
-            queue = deque()
-            queue.append((row, col, visited))
+        def dfs(row, col, visited, maxCount):
+            stack = []
+            stack.append((row, col, visited))
             visited.add((row, col))
             count = 1
 
-            while queue:
-                
-                #Pop
-                row, col, visited = queue.popleft()
+            while stack:
+
+                row, col, visited = stack.pop()
 
                 for nr, nc in directions:
-                    newRow = row + nr
-                    newCol = col + nc
+                    newRow = nr + row
+                    newCol = nc + col
 
                     if 0 <= newRow < rows and 0 <= newCol < cols and grid[newRow][newCol] == 1 and (newRow, newCol) not in visited:
-                        visited.add((newRow, newCol))
-                        queue.append((newRow, newCol, visited))
                         count += 1
+                        visited.add((newRow, newCol))
+                        stack.append((newRow, newCol, visited))
             
             return count
 
         for row in range(rows):
             for col in range(cols):
                 if grid[row][col] == 1 and (row, col) not in visited:
-                    count = bfs(row, col, visited, maxCount)
+                    count = dfs(row, col, visited, maxCount)
                     maxCount = max(maxCount, count)
         
         return maxCount
