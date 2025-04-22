b04a8c04cac8fef9c581fb5489c8fe449f194ac4
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-04-22 17:01:27 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,38 +1,41 @@
 from collections import deque
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        
-        def bfs(row, col, visited, directions):
-            queue = deque()
-            queue.append((row, col))
-            visited.add((row, col))
-            area = 1
-
-            while queue:
-                currRow, currCol = queue.pop()
-
-                for nRow, nCol in directions:
-                    newRow = nRow + currRow
-                    newCol = nCol + currCol
-
-                    if 0 <= newRow < rows and 0 <= newCol < cols and grid[newRow][newCol] == 1 and (newRow, newCol) not in visited:
-                        visited.add((newRow, newCol))
-                        queue.append((newRow, newCol))
-                        area += 1
-            
-            return area
 
         rows = len(grid)
         cols = len(grid[0])
         visited = set()
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)] #Left, Right, Up, Down
-        returnRes = 0
+        directions = [(0, -1), (0, 1), (-1, 0), (1, 0)] #Up, Down, Right, Left
+        print(rows)
+        print(cols)
+        res = 0
+
+        def bfs(row, col, visited):
+            queue = deque()
+            queue.append((row, col, visited))
+            visited.add((row, col))
+
+            while queue:
+                
+                #Pop
+                row, col, visited = queue.popleft()
+
+                for nr, nc in directions:
+                    newRow = row + nr
+                    newCol = col + nc
+
+                    if 0 <= newRow < rows and 0 <= newCol < cols and grid[newRow][newCol] == 1 and (newRow, newCol) not in visited:
+                        visited.add((newRow, newCol))
+                        queue.append((newRow, newCol, visited))
 
         for row in range(rows):
             for col in range(cols):
-                if grid[row][col] == 1:
-                    returnRes = max(returnRes, bfs(row, col, visited, directions))
+                if grid[row][col] == 1 and (row, col) not in visited:
+                    res += 1
+                    bfs(row, col, visited)
         
-        return returnRes
-        
+        return res
 
+
+        
+                
