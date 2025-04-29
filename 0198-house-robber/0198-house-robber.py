8b64ece99d9b3dac20ce70aeff4599470d519d0c
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-04-29 18:00:06 -0500

    Time: 0 ms (100%), Space: 17.8 MB (34.43%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,12 +1,30 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
 
-        rob1, rob2 = 0, 0
 
-        for i in nums:
-            temp = max(i + rob1, rob2)
-            rob1 = rob2
-            rob2 = temp
-        return rob2
+        my_dict = {}
 
+        def robHouse(i):
+
+            #Base Case
+            if i > len(nums) - 1:
+                return 0
+
+            #Recursive WITH DP
+            if i in my_dict:
+                return my_dict[i]
+
+            #Rob
+            robIt = nums[i] + robHouse(i + 2)
+
+            #Skip
+            skipIt = robHouse(i + 1)
+            
+            my_dict[i] = max(robIt, skipIt)
+
+            return my_dict[i]
+
+
+        return robHouse(0)
+        
         
