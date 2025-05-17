202a9fecd3ee7a423e7bfa8606f98737e0f87ca8
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-05-17 08:15:51 -0500

    Time: 0 ms (100%), Space: 17.9 MB (33.54%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,19 +1,20 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
+
+        my_dict = {0: 0, 1: 0, 2: 0}
+        nums2 = []
+
+        for i in range(len(nums)):
+            if nums[i] in my_dict:
+                my_dict[nums[i]] += 1
         
-        left = 0
+        
+        index = 0
+        for color in my_dict:
+            for _ in range(my_dict[color]):
+                nums[index] = color
+                index += 1
 
-        for color in range(len(nums)):
-            minNum = float('inf')
-            index = 0
-            for right in range(color, len(nums)):
-
-                if nums[right] < minNum:
-                    minNum = nums[right]
-                    index = right
-            
-            temp = nums[color]
-            nums[color] = minNum
-            nums[index] = temp
-                
-
+        
+        
+        
