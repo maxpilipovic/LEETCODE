283f1270983a529dc576e490469b86680857ab5a
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-05-31 15:13:10 -0500

    Time: 1543 ms (8.36%), Space: 309.6 MB (13.48%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,16 +1,26 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        #DP Solution
+        #MEMOIZATION SOLUTION
 
-        dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
+        memo = {}
 
-        for i in range(len(text1) - 1, -1, -1):
-            for j in range(len(text2) - 1, -1, -1):
+        def recursion(i, j):
 
-                if text1[i] == text2[j]:
-                    dp[i][j] = 1 + dp[i + 1][j + 1]
-                else:
-                    dp[i][j] = max(dp[i + 1][j], dp[i][j+1])
-        
-        return dp[0][0]
+            #Base Case
+            if i == len(text1) or j == len(text2):
+                return 0
+            
+            #Check result in memo
+            if (i, j) in memo:
+                return memo[(i, j)]
+            
+            if text1[i] == text2[j]:
+                result = 1 + recursion(i + 1, j + 1)
+            else:
+                result = max(recursion(i + 1, j), recursion(i, j + 1))
+
+            memo[(i, j)] = result
+            return result
+
+        return recursion(0, 0)
 
