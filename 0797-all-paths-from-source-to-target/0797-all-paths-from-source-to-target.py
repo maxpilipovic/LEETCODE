b2c391dfb0680674888b5730558650df882f7111
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-06-01 17:55:31 -0500

    Time: 8 ms (17.85%), Space: 19.3 MB (31.6%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,41 +1,44 @@
-from collections import deque
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        #DFS
-        def adjList(graph):
 
-            adjList = {}
-
-            for i, neighbors in enumerate(graph):
-
-                if i not in adjList:
-                    adjList[i] = neighbors
-
-            return adjList
-
-        def bfs(adjList, node, dest, path, res):
-
-            queue = deque([(node, path)])
-
-            while queue:
-                node, path = queue.popleft()
-
-                if node == dest:
-                    res.append(path[:])
-                
-                for neighbor in adjList[node]:
-                    queue.append([neighbor, path + [neighbor]])
-                    
-        adjList = adjList(graph)
-        dest = len(graph) - 1
         res = []
-        bfs(adjList, 0, dest, [0], res)
+
+        def adjList(edges):
+            
+            graph = {}
+
+            #Nodes
+            for i in range(len(edges)):
+                graph[i] = edges[i]
+            
+            return graph
+        
+        def dfs(node, array, targetNode, visited):
+            
+            #Append
+            array.append(node)
+            visited.add(node)
+
+            #Base Case
+            if node == targetNode:
+                res.append(array.copy())
+                array.pop()
+                visited.remove(node)
+                return
+
+            #Loop through neighbors
+            for neighbor in adjList[node]:
+                if neighbor not in visited:
+                    dfs(neighbor, array, targetNode, visited)
+            
+            array.pop()
+            visited.remove(node)
+            
+        targetNode = len(graph) - 1
+        adjList = adjList(graph)
+        print(adjList)
+        visited = set()
+
+        dfs(0, [], targetNode, visited)
 
         return res
-        
-        
-        
-                
-
-
-        
