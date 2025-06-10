dd1a158bd41a84c5fcbf37402a20cf6fb5f9474d
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-06-09 19:03:30 -0500

    Time: 127 ms (39.27%), Space: 50.1 MB (83.73%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -1,19 +1,17 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         
-        sortedVals = sorted(intervals, key=lambda x: x[1])
-        print(sortedVals)
-
+        #Sort
+        sortedIntervals = sorted(intervals, key=lambda x: x[1])
+        print(sortedIntervals) 
         count = 0
-        end = sortedVals[0][1] #Sets end to be 'end value' of the pair
 
-        for i in range(1, len(sortedVals)):
-            currStart, currEnd = sortedVals[i]
-
-            if currStart < end:
+        firstTuple = sortedIntervals[0]
+        for i in range(1, len(sortedIntervals)):
+            
+            if firstTuple[1] > sortedIntervals[i][0]:
                 count += 1
             else:
-                end = currEnd
-            
+                firstTuple = sortedIntervals[i]
         
         return count
