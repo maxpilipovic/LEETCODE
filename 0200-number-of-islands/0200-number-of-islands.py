af73fab26eb17aaf5100ae5a78f39170a9a0f87b
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-06-13 18:01:22 -0500

    Time: 266 ms (35.19%), Space: 25.3 MB (26.64%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,44 +1,36 @@
 from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-
+        
         rows = len(grid)
         cols = len(grid[0])
+        directions = [(0, -1), (0, 1), (1,0), (-1, 0)] #Up, Down, Right, Left
         visited = set()
-        directions =[(0, -1), (0, 1), (-1, 0), (1, 0)] #Up, Down, Left, Right
-        islandCount = 0
+        count = 0
 
-
-        print(rows)
-        print(cols)
-
-        def bfs(row, col, visited, directions):
+        def bfs(r, c, visited, directions):
+            
             queue = deque()
-            queue.append((row, col, visited))
+            queue.append((r, c))
+            visited.add((r, c))
 
             while queue:
-                row, col, visited = queue.pop()
 
-                for nRow, nCol in directions:
-                    newRow = row + nRow
-                    newCol = col + nCol
+                row, col = queue.popleft()
 
-                    if 0 <= newRow < rows and 0 <= newCol < cols and grid[newRow][newCol] == '1' and (newRow, newCol) not in visited:
+                for nr, nc in directions:
+                    newRow = nr + row
+                    newCol = nc + col
+
+                    if 0 <= newRow < rows and 0 <= newCol < cols and (newRow, newCol) not in visited and grid[newRow][newCol] == "1":
                         visited.add((newRow, newCol))
-                        queue.append((newRow, newCol, visited))
+                        queue.append((newRow, newCol))
+
 
         for row in range(rows):
             for col in range(cols):
-                if grid[row][col] == '1' and (row, col) not in visited:
-                    print("Getting here")
-                    islandCount += 1
-                    visited.add((row, col))
+                if grid[row][col] == "1" and (row, col) not in visited:
+                    count += 1
                     bfs(row, col, visited, directions)
-
-        return islandCount
-
-        
-
-
-
-        
+            
+        return count
