9d5d7383e1d65553f8fb0ff949f40d9e75548d9d
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-06-15 16:49:50 -0500

    Time: 24 ms (49.3%), Space: 44.5 MB (89.58%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
@@ -1,15 +1,31 @@
 class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
         
+        def adjList(edges):
+
+            graph = {}
+
+            for edge in edges:
+
+                a,b = edge
+
+                if a not in graph:
+                    graph[a] = []
+                if b not in graph:
+                    graph[b] = []
+
+                graph[a].append(b)
+
+            return graph
+        
         res = []
         hashy = set()
+        for incomingEdge, outgoingEdge in edges:
 
-        for a, b in edges:
-            hashy.add(b)
+            hashy.add(outgoingEdge)
         
-        print(hashy)
-
         for i in range(n):
+
             if i not in hashy:
                 res.append(i)
         
