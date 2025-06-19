b6fa71b3629517af0ee4157cb5cad7dafdf1304f
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-06-18 21:08:34 -0500

    Time: 9 ms (29.81%), Space: 21.1 MB (80.06%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,16 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        freqMap = Counter(nums)
+        print(freqMap)
 
-        my_dict = dict()
+        heap = []
 
-        for i in range(len(nums)):
-            if nums[i] in my_dict:
-                my_dict[nums[i]] += 1
-            else:
-                my_dict[nums[i]] = 1
-            
-        sorted_items = sorted(my_dict.items(), key=lambda x: x[1], reverse=True)
+        for key, value in freqMap.items():
+            heapq.heappush(heap, (value, key))
 
-        top = [item[0] for item in sorted_items[:k]]
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        #List comprehension
+        return [v for _, v in heap]
 
-        return top
