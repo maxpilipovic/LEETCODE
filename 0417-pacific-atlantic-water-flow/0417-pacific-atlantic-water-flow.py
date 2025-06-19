8d7554937b3d7392ac80685a4fe7adef20781b53
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-06-18 20:32:20 -0500

    Time: 1951 ms (5%), Space: 19.5 MB (46.52%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -5,48 +5,58 @@ class Solution:
         res = []
         rows = len(heights)
         cols = len(heights[0])
-        directions = [(0, 1), (0, -1), (-1, 0), (1, 0)] #Up, Down, Left, Right
+        directions = [(0, -1), (0, 1), (-1, 0), (1, 0)] #Up, Down, Left, Right
 
-
-        def bfs(row, col):
+        def bfs(r, c, visited, directions):
+            
             queue = deque()
-            queue.append((row, col))
-            visited = set()
-            visited.add((row, col))
-
-            pOcean = False
-            aOcean = False
+            queue.append((r, c))
+            visited.add((r, c))
+            pacificOcean = False
+            atlanticOcean = False
 
             while queue:
-                row, col = queue.popleft()
 
-                #Check if cell borders ocean
+
+                row, col  = queue.popleft()
+
+                #Check our flags.
+
+                #Pacific
                 if row == 0 or col == 0:
-                    pOcean = True
-                if row == rows - 1 or col == cols - 1:
-                    aOcean = True
+                    pacificOcean = True
                 
-                if pOcean and aOcean:
+                #Atlantic
+                if row == rows - 1 or col == cols - 1:
+                    atlanticOcean = True
+                
+                #Check if they hit.
+                if pacificOcean and atlanticOcean:
                     return True
 
-                #Loop through neighbors
                 for nr, nc in directions:
+
                     newRow = nr + row
                     newCol = nc + col
 
-                    if 0 <= newRow < rows and 0 <= newCol < cols and heights[newRow][newCol] <= heights[row][col] and (newRow, newCol) not in visited:
+                    #Check bounds
+                    if 0 <= newRow < rows and 0 <= newCol < cols and (newRow, newCol) not in visited and heights[row][col] >= heights[newRow][newCol]:
 
                         queue.append((newRow, newCol))
                         visited.add((newRow, newCol))
             
             return False
 
-        for row in range(rows):
-            for col in range(cols):
-               if bfs(row, col):
-                    res.append([row, col]) 
+        for r in range(rows):
+            for c in range(cols):
+                if bfs(r, c, set(), directions):
+                    res.append((r, c))
+
         
         return res
+            
+        
+
 
 
         
