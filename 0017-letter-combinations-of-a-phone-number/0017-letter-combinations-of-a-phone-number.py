dca9a51d9dae772d5ed4ce46f74b437d6f356224
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-06-19 19:06:21 -0500

    Time: 0 ms (100%), Space: 17.8 MB (41.36%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -3,33 +3,33 @@ class Solution:
         
         if not digits:
             return []
-
+            
         res = []
-        graph = {
-            2: ["a", "b", "c"],
-            3: ["d", "e", "f"],
-            4: ["g", "h", "i"],
-            5: ["j", "k", "l"],
-            6: ["m", "n", "o"],
-            7: ["p", "q", "r", "s"],
-            8: ["t", "u", "v"],
-            9: ["w", "x", "y", "z"]
+        my_dict = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
         }
 
-        def backtrack(graph, index, curr):
-
+        def backtrack(i, curr):
+            
             #Base Case
-            if index == len(digits):
-                res.append("".join(curr.copy()))
+            if len(curr) == len(digits):
+                res.append("".join(curr))
                 return
 
-            #Recursive
-            for neighbor in graph[int(digits[index])]:
-                curr.append(neighbor)
-                backtrack(graph, index + 1, curr)
+            for num in my_dict[digits[i]]:
+                curr.append(num)
+                backtrack(i + 1, curr)
                 curr.pop()
         
-
-        backtrack(graph, 0, [])
+        backtrack(0, [])
 
         return res
+
+
