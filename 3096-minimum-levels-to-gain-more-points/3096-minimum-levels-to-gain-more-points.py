1e3709d84eaadc9c3b11823823405e66e3c538f1
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-07-07 20:01:19 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3096-minimum-levels-to-gain-more-points/3096-minimum-levels-to-gain-more-points.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minimumLevels(self, possible: List[int]) -> int:
+        
+        #Change everything into -1
+        for i in range(len(possible)):
+            if possible[i] == 0:
+                possible[i] = -1
+
+        #prefixDiff = 0
+        totalSum = sum(possible)
+        num = 1
+        while num < len(possible):
+            
+            #Prefix Sum
+            #Slice
+            alicePart = possible[:num]
+            sumAlice = sum(alicePart)
+            sumBob = totalSum - sumAlice
+            
+
+            #Sum
+            #sumAlice = sum(alicePart)
+            #sumBob = sum(bobPart)
+
+            if sumAlice > sumBob:
+                return num
+        
+            num += 1
+        
+        return -1
