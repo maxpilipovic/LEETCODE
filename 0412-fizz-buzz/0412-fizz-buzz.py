0d6e29ca73ab9e4c42ee4c2ef00c35a4ac319515
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-08-12 12:00:40 -0500

    Time: 0 ms (100%), Space: 18.2 MB (69.57%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,10 +1,11 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-
+        
         i = 1
         res = []
+
         while i <= n:
-            
+
             if i % 3 == 0 and i % 5 == 0:
                 res.append("FizzBuzz")
             elif i % 3 == 0:
