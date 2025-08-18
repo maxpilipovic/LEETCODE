6af325a22e84d9dde648790abbf1ed76095b57c9
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-08-18 18:40:48 -0500

    Time: 11 ms (14.07%), Space: 19.5 MB (21.86%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,54 +1,56 @@
+from collections import defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        def adjacencyList(edges):
 
-        def adjList(edges):
-
-            graph = {}
+            graph = defaultdict(list)
 
             for edge in edges:
+
                 a,b = edge
 
-                if a not in graph:
-                    graph[a] = []
                 if b not in graph:
                     graph[b] = []
+                if a not in graph:
+                    graph[a] = []
 
+                #Add
                 graph[b].append(a)
             
             return graph
-        
-        def dfs(node, visited, path, graph):
-            
-            if node in path: #Cycle detection
-                return True
 
-            if node in visited: #Speed up.
-                return False 
-            
-            path.add(node)
-
-            for neighbor in graph.get(node, []):
-                if dfs(neighbor, visited, path, graph):
-                    return True
-            
-            path.remove(node)
-            visited.add(node)
-
-            return False
-
-        
-
-        graph = adjList(prerequisites)
+        graph = adjacencyList(prerequisites)
+        print(graph)
         visited = set()
 
-        print(graph)
+        #DFS
+        def dfs(node, visited, path):
+            
+            #Base Case
+            if node in path:
+                return False
+            
+            if node in visited:
+                return True
+            
+            path.add(node)
+            #Recursive
+            for neighbor in graph[node]:
+                
+                if not dfs(neighbor, visited, path):
+                    return False
+            
+            #Backtrack
+            path.remove(node)
+            #Fully explored.
+            visited.add(node)
 
+            return True
 
-        for node in range(numCourses - 1):
-            if dfs(node, visited, set(), graph):
+        for node in range(numCourses):
+            print(node)
+            if not dfs(node, visited, set()):
                 return False
         
         return True
-
-
-        
