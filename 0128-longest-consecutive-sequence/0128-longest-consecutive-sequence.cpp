4e8041a1e2fdd29fd2ea9fb0748811ee23afec70
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-10-01 14:58:14 -0500

    Time: 11 ms (93.94%), Space: 60 MB (98.6%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -6,36 +6,34 @@ public:
         {
             return 0;
         }
-        
+
         std::sort(nums.begin(), nums.end());
-        
-        int res = 1; //biggest val
-        int streak = 1;
 
         for (int i = 1; i < nums.size(); i++)
         {
-            if (nums[i] == nums[i - 1])
+            if (nums[i] == nums[i - 1] + 1)
+            {
+                streak += 1;
+            }
+            else if (nums[i] == nums[i - 1])
             {
                 //Duplicate
                 continue;
             }
-            else if (nums[i] == nums[i - 1] + 1)
-            {
-                streak += 1;
-            }
             else {
-                //Reset
                 streak = 1;
             }
-            
-            //Update max.
+
             if (streak > res)
             {
                 res = streak;
             }
-            
-        } 
+        }
 
         return res;
     }
+
+private:
+    int res = 1;
+    int streak = 1;
 };
