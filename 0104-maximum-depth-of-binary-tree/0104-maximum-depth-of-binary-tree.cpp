b85e4c1b9606a31438441dc6176b7efe631a0a89
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-10-01 17:19:40 -0500

    Time: 0 ms (100%), Space: 19.2 MB (13.45%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -9,44 +9,44 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
- #include <stack>
+#include <stack> 
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-
-        //Iterative...
-        //Recursive
-
+        
         if (!root)
         {
             return 0;
         }
 
-        std::stack<std::pair<TreeNode*, int>> stack;
-        stack.push(std::make_pair(root, 1));
+        stack<pair<TreeNode*, int>> stack;
+        stack.push({root, 1});
 
         while (!stack.empty())
         {
 
-            auto [curr, count] = stack.top();
+            auto [node, count] = stack.top();
             stack.pop();
 
-            //Check our max
-            resMax = std::max(resMax, count);
-
-            if (curr->left)
+            if (node->left)
             {
-                stack.push(std::make_pair(curr->left, count + 1));
+                stack.push({node->left, count + 1});
             }
-            if (curr->right)
+
+            if (node->right)
             {
-                stack.push(std::make_pair(curr->right, count + 1));
+                stack.push({node->right, count + 1});
+            }
+
+            if (count > res)
+            {
+                res = count;
             }
         }
 
-        return resMax;
-    }
+        return res;
 
+    }
 private:
-    int resMax = 0;
+    int res = 0;
 };
