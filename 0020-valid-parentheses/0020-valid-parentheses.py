0df0bfe6b68a56c92c2d7e1423f4c884fa559bd9
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-10-14 17:38:29 -0500

    Time: 2 ms (39.47%), Space: 17.9 MB (57.09%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,44 +1,26 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        
+        if len(s) == 1:
+            return False
+
+        flag = False
         stack = []
-        openCount = 0
-        openCount2 = 0
-        openCount3 = 0
 
-        for i in s:
-            if i == "(":
-                stack.append(i)
-                openCount += 1
-            elif i == "{":
-                stack.append(i)
-                openCount2 += 1
-            elif i == "[":
-                stack.append(i)
-                openCount3 += 1
-            elif i == ")":
-                if openCount > 0 and stack and stack[-1] == "(":
-                    stack.pop()
-                    openCount -= 1
-                else:
-                    return False
-            elif i == "}":
-                if openCount2 > 0 and stack and stack[-1] == "{":
-                    stack.pop()
-                    openCount2 -= 1
-                else:
-                    return False
-            elif i == "]":
-                if openCount3 > 0 and stack and stack[-1] == "[":
-                    stack.pop()
-                    openCount3 -= 1
-                else:
+        #hashmap
+        my_dict = {")" : "(", "}" : "{", "]" : "["}
+
+        for char in s:
+
+            if char in my_dict.values():
+                stack.append(char)
+            elif char in my_dict: #If we its a closing bracket..
+                if not stack or stack.pop() != my_dict[char]:
+                    flag = True
                     return False
         
-        return openCount == 0 and openCount2 == 0 and openCount3 == 0
-
-
+        return len(stack) == 0
         
 
-
-
-        
+        #Everytime we see an opeing bracket. We want to append...
+        #Ok is the stack empty? or the value at the topf of the stack is the my_dict[char] -> value...
