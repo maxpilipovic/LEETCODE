f1c6b1762866b238c39d21ffda87761c1ac75247
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-10-21 17:53:39 -0500

    Time: 0 ms (100%), Space: 17.7 MB (72.2%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -7,24 +7,30 @@
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
         
-        result = []
+        #Check if root is empty
+        if not root:
+            return []
 
-        if root is None:
-            return result
+        res = []
+        stack = [(root, str(root.val))]
+
+        while stack:
+            node, path = stack.pop()
+            
+            #If its a leaf
+            if not node.left and not node.right:
+                res.append(path)
+            
+            if node.right:
+                stack.append((node.right, path + "->" +str(node.right.val)))
+            if node.left:
+                stack.append((node.left, path + "->" + str(node.left.val)))
         
-        self.dfs(root, str(root.val), result)
-        return result
-        
-    def dfs(self, node, current_path, result):
+        return res
 
-        if node.left is None and node.right is None:
-            result.append(current_path)
-            return
-
-        if node.left is not None:
-            self.dfs(node.left, current_path + '->' + str(node.left.val), result)
-
-        if node.right is not None:
-            self.dfs(node.right, current_path + '->' + str(node.right.val), result)
 
             
+
+
+
+        
