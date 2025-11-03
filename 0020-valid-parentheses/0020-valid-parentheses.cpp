1863d4d670a7e9afcbd69ecd2996ea96ae567d87
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-11-03 11:31:56 -0600

    Time: 1 ms (18.78%), Space: 8.9 MB (66.37%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
-    bool isValid(string s) {
+    bool isValid(std::string s) {
         
         std::stack<char> stack;
 
-        for (char c : s)
+        for (int i = 0; i < s.size(); i++)
         {
-            //Closing bracket
-            if (map.count(c)) //If it exists
+            if (!stack.empty() && (s[i] == '}' || s[i] == ')' || s[i] == ']'))
             {
-                if (stack.empty() || stack.top() != map[c])
+                char c = stack.top();
+                
+                switch (s[i])  // switch on the current closing bracket
                 {
-                    return false;
+                    case ')':
+                        if (c != '(') return false;
+                        break;
+                    case '}':
+                        if (c != '{') return false;
+                        break;
+                    case ']':
+                        if (c != '[') return false;
+                        break;
                 }
+
                 stack.pop();
             }
             else
             {
-                stack.push(c);
+                stack.push(s[i]);
             }
         }
-        
-        //Check if empty. Has to be equal
-        if (stack.empty())
-        {
-            return true;
-        }
 
-        return false;
+        return (stack.size() == 0);
     }
-
-private:
-    unordered_map<char, char> map = {{')', '('}, {'}', '{'}, {']', '['}};
 };
