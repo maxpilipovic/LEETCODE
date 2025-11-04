0fa34742fa14a817053ecc15cf4bf35d41e2abee
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-11-04 17:26:04 -0600

    Time: 356 ms (21.18%), Space: 17.8 MB (41%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,10 +1,9 @@
 class Solution:
     def fib(self, n: int) -> int:
         
+        #Base Case
         if n == 0 or n == 1:
             return n
-
-        return self.fib(n - 1) + self.fib(n - 2)
-
-
         
+        #Recursive Case
+        return self.fib(n-1) + self.fib(n-2)
