5f82374fa8848176af6701c11c66b34bb482d4d1
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-11-04 17:36:20 -0600

    Time: 5 ms (86.18%), Space: 17.8 MB (60.94%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,23 +1,30 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-
+        
         res = []
 
-        def backtrack(start, currSum, curr):
+        def backtrack(start,  currSum, curr):
+
             #Base Case
             if currSum == target:
                 res.append(curr.copy())
-                return
-                
+                return 
+            
+            #currSUm > target. RETURN. THIS DOES NOT WORK. 
             if currSum > target:
                 return
 
+            #Recursive backtracking..
             for i in range(start, len(candidates)):
-
                 curr.append(candidates[i])
+
+                #Call our function to backtrack
                 backtrack(i, currSum + candidates[i], curr)
+
+                #Backtrack...
                 curr.pop()
-            
+        
+
         backtrack(0, 0, [])
 
         return res
