399d77d743c8ab606a6f9c6b0a86c258dbb4458b
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2025-11-13 16:04:34 -0600

    Time: 5 ms (54.38%), Space: 15.8 MB (54.38%) - LeetHub

---
+++ 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxOperations(string s) {
+        
+        int n = s.size();
+        int countOnes = 0;
+        //bool flag = false;
+
+        for (int i = 0; i < n; i++)
+        {
+
+            if (s[i] == '1')
+            {
+                countOnes += 1;
+            }
+
+            if (s[i] == '1' && s[i + 1] == '0')
+            {
+                res += countOnes;
+                //countOnes = 0;
+            }
+
+        }
+
+        return res;
+    }
+
+private:
+    int res = 0;
+};
