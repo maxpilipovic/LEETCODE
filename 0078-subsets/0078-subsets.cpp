e8946899bc54c24055a6c193d1622774812e3e35
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2026-02-05 20:56:11 -0600

    Time: 3 ms (31.71%), Space: 15.8 MB (10.02%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        
-        size_t n = nums.size();
-        std::vector<std::vector<int>> res;
+    vector<vector<int>> subsets(vector<int>& nums) 
+    {
+        std::vector<int> curr;
 
-        for (size_t mask = 0; mask < 1 << n; mask++)
-        {
-            std::vector<int> curr;
-            for (size_t i = 0; i < n; i++)
-            {
-                //
-                // -> 0001
-                //1
-                //2
-                //mask & (1 << i)
-                if (mask >> i & 1)
-                {
-                    curr.push_back(nums[i]);
-                }
-            }
-            res.push_back(std::move(curr));
-        }
+        dfs(nums, curr, 0);
 
         return res;
-    }   
+    }
+
+    void dfs(vector<int>& nums, vector<int> curr, int i)
+    {
+
+        //Base Case
+        if (i >= nums.size())
+        {
+            res.push_back(curr);
+            return;
+        }
+
+        //Add
+        curr.push_back(nums[i]);
+
+        //Take 
+        dfs(nums, curr, i + 1);
+
+        //Skip
+        curr.pop_back();
+        dfs(nums, curr, i + 1);
+        
+    }
+
+private:
+    vector<vector<int>> res;
 };
