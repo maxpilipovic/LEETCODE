1c8047d8fdba1fc1c7f36c33c7fe7a820808261a
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2026-02-12 21:31:08 -0600

    Time: 0 ms (100%), Space: 8.2 MB (63.45%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -1,54 +1,52 @@
 class Solution {
 public:
-    int compareVersion(string version1, string version2) {
-
+    int compareVersion(string version1, string version2) 
+    {
+        
         int v1Size = version1.size();
         int v2Size = version2.size();
+
         int i = 0;
         int j = 0;
 
-        //Two Pointer...
         while (i < v1Size || j < v2Size)
         {
             long long num1 = 0;
             long long num2 = 0;
 
-            while (i < v1Size && version1[i] != '.')
+            while (version1[i] != '.' && i < v1Size)
             {
-                //Convert to long long. Not sure...
                 num1 = num1 * 10 + (version1[i] - '0');
                 i++;
             }
 
-            while (j < v2Size && version2[j] != '.')
+            while (version2[j] != '.' && j < v2Size)
             {
-                //Convert to float. Not sure...
                 num2 = num2 * 10 + (version2[j] - '0');
                 j++;
             }
 
-            cout << num1 << endl;
-            cout << num2 << endl;
-
-            //Compare our num1 and num2.
+            if (num1 < num2)
+            {
+                return -1;
+            }
             if (num1 > num2)
             {
                 return 1;
             }
-            else if (num1 < num2)
+
+            //Skip remainde
+            if (i < v1Size && version1[i] == '.')
             {
-                return -1;
+                i++;
+            }
+            if (j < v2Size && version2[j] == '.')
+            {
+                j++;
             }
 
-            //Increase... skips '.'
-            i++;
-            j++;
         }
 
         return 0;
     }
-
-
-private:
-    int res;
 };
