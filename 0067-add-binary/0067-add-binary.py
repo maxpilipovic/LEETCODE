c8bb958360a72a13ba8457cc8037c3de274128c3
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2026-02-15 15:44:54 -0600

    Time: 3 ms (44.43%), Space: 17.8 MB (69.85%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,12 +1,50 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-
-        num1 = int(a, 2)
-        num2 = int(b, 2)
-
-        total = num1 + num2
-
-        res = bin(total)[2:]
-
-        return res
         
+        i = len(a) - 1
+        j = len(b) - 1
+        carry = 0
+        resStr = ""
+        
+        while i >= 0 or j >= 0 or carry:
+            #Convert int
+            #2. Bounds check?
+
+            if i < 0:
+                bitA = 0
+            else:
+                bitA = int(a[i])
+
+            if j < 0:
+                bitB = 0
+            else:
+                bitB = int(b[j])
+            
+            #0, 1, 2, 3
+            total = bitA + bitB + carry
+
+            if total == 0:
+                resStr += str(total)
+                carry = 0
+            elif total == 1:
+                resStr += str(total)
+                carry = 0
+            elif total == 2:
+                resStr += str(0)
+                carry = 1
+            elif total == 3:
+                resStr += str(1)
+                carry = 1
+
+            i -= 1
+            j -= 1
+
+        return resStr[::-1]
+        
+
+
+
+
+
+
+
