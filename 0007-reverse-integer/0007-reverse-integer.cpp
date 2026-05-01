8d943b67aa6014ed9589db84d520b534bd2954f9
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2026-04-30 23:40:51 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    int reverse(int x) {
-        
-        int64_t res = 0;
-        int64_t MAX = std::numeric_limits<int>::max();
-        int64_t MIN = std::numeric_limits<int>::min();
+    int reverse(int x) 
+    {
+        int res = 0;
 
         while (x != 0)
         {
             int digit = x % 10;
             x /= 10;
 
-            res = (res * 10) + digit;
+            //Check for overflow BEFORE multiplying
+            if (res > INT_MAX / 10 || res < INT_MIN / 10)
+                return 0;
+
+            res = res * 10 + digit;
         }
 
-        //Figure out our bounds
-        if (res > MAX || res < MIN)
-        {
-            return 0;
-        }
-
-        return res;        
-
+        return res;
     }
 };
