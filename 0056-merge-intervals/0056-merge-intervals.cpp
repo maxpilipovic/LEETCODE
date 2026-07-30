5bd4c54eae8c1c25be2ba35218b0a34123bae7cc
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2026-07-30 16:00:43 -0500

    Time: 8 ms (23.68%), Space: 23.7 MB (95.61%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        
-        if (intervals.empty())
+    vector<vector<int>> merge(vector<vector<int>>& intervals) 
+    {
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end());  //Sort by first element lowkey
+        vector<vector<int>> res;
+
+        for (auto& interval : intervals)
         {
-            return {};
-        }
-
-        std::sort(intervals.begin(), intervals.end());
-
-        res.push_back(intervals[0]);
-
-        for (int i = 1; i < intervals.size(); i++)
-        {
-            
-            if (intervals[i][0] <= res.back()[1])
+            if (!res.empty() && interval[0] <= res.back()[1])
             {
-                res.back()[1] = intervals[i][1];
+                res.back()[1] = max(res.back()[1], interval[1]);
             }
-            else 
+            else
             {
-                res.push_back(intervals[i]);
+                res.push_back(interval);
             }
         }
+
         return res;
     }
-
-private:
-    vector<vector<int>> res;
 };
