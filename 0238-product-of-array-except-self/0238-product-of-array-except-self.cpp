d38e153f6c82ca99ebcf3436cd5392276f3c8cbe
Max Pilipovic <84630222+maxpilipovic@users.noreply.github.com>
2026-08-25 15:59:06 -0500

    Time: 866 ms (5.14%), Space: 40 MB (95.94%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,26 +1,42 @@
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        
-        
-
+    vector<int> productExceptSelf(vector<int>& nums) 
+    {
         vector<int> res(nums.size(), 1);
 
         int left = 1;
-        //Two passes O(n)
-        for (int i = 0; i < nums.size(); i++)
+
+        for (int i{}; i < nums.size(); i++)
         {
             res[i] = left;
             left *= nums[i];
         }
 
+        printVector(res);
+
         int right = 1;
+
         for (int i = nums.size() - 1; i >= 0; i--)
         {
             res[i] *= right;
             right *= nums[i];
         }
 
+        printVector(res);
+        
         return res;
     }
+
+    void printVector(const vector<int>& v) 
+    {
+        cout << "[";
+        for (size_t i = 0; i < v.size(); ++i) {
+            cout << v[i];
+            if (i + 1 < v.size()) cout << ", ";
+        }
+        cout << "]" << endl;
+    }
+
+private:
+
 };
